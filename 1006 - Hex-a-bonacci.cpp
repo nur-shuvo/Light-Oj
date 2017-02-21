fd94f1151ad75ca09b5890b5955c4e3d49de6944
Nur Shuvo <nurshuvo51@gmail.com>
2017-02-21 21:54:35 +0600

    Add files via upload

--- 1006 - Hex-a-bonacci.cpp
+++ 1006 - Hex-a-bonacci.cpp
@@ -1,14 +1,31 @@
 #include<stdio.h>
+
+long long dp[10005];
+long long ff(long long n)
+{
+    if( n == 0 ) return dp[0];
+    if( n == 1 ) return dp[1];
+    if( n == 2 ) return dp[2];
+    if( n == 3 ) return dp[3];
+    if( n == 4 ) return dp[4];
+    if( n == 5 ) return dp[5];
+
+    if(dp[n]!=-1){
+        return dp[n];
+    }
+    else {
+       return dp[n] = ( ff(n-1) + ff(n-2) + ff(n-3) + ff(n-4) + ff(n-5) + ff(n-6) ) % 10000007;
+    }
+}
+
 int main() {
-    long long n,caseno = 0,cases,fn[10005];
+    long long n,caseno = 0,cases;
     scanf("%lld",&cases);
     while( cases-- ) {
-        scanf("%lld%lld%lld%lld%lld%lld%lld",&fn[0],&fn[1],&fn[2],&fn[3],&fn[4],&fn[5],&n);
-
-          for(int i=6;i<=n;i++)
-          fn[i]=(fn[i-1]+fn[i-2]+fn[i-3]+fn[i-4]+fn[i-5]+fn[i-6])%10000007;
-
-          printf("Case %lld: %lld\n", ++caseno, fn[n] % 10000007);
+          for(long long i=0;i<10005;i++) dp[i]=-1;
+          scanf("%lld%lld%lld%lld%lld%lld%lld",&dp[0],&dp[1],&dp[2],&dp[3],&dp[4],&dp[5],&n);
+          ff(n);
+          printf("Case %lld: %lld\n", ++caseno,dp[n]%10000007 );
     }
     return 0;
 }
